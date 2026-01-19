2dcf15be704a0bc6264bfec0cd58bb53fa2de1f7
Rebecca Ingulkar <rebeccaingulkar@outlook.com>
2026-01-20 10:56:36 +1300

    Rename getFruits to getCafeSummaries function

--- client/apis/fruits.ts
+++ client/apis/fruits.ts
@@ -2,7 +2,7 @@ import request from 'superagent'
 
 const rootURL = new URL(`/api/v1`, document.baseURI)
 
-export async function getFruits(): Promise<string[]> {
+export async function getCafeSummaries(): Promise<Cafe> {
   const response = await request.get(`${rootURL}/fruits`)
   return response.body.fruits as string[]
 }
